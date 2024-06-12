9eadd9b77ba0a85e4cbe28f5d4e1d878006595ca
jihe05 <163379277+jihe05@users.noreply.github.com>
2024-06-13 02:11:55 +0900

    InputSystem 플레이어

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -8,18 +8,41 @@ using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
+    public float speed = 3f;
+    private Vector3 moveInput;
+    private Animator animator;
 
-    public Animator moveAnimator;
-
-    private void Update()
+    private void Awake()
     {
-      
-    }
-
-    public void OnMove()
-    {
+        animator = GetComponent<Animator>();
         
     }
 
+    public void OnMove(InputValue inputValue)
+    {
+        Debug.Log("°£´ç!");
+        animator.SetBool("isMove", true);
+        moveInput = inputValue.Get<Vector2>();
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        Debug.Log($"X¿Í Y :{moveInput.x},{moveInput.y}");
+
+        Vector3 movement = new Vector2(moveInput.x, moveInput.y) * speed * Time.fixedDeltaTime;
+
+        animator.SetFloat("MoveX", moveInput.x);
+        animator.SetFloat("MoveY", moveInput.y);
+
+        if (moveInput.x == 0 && moveInput.y == 0)
+        {
+            animator.SetBool("isMove", false);
+        }
+
+    }
+
+   
 
 }
