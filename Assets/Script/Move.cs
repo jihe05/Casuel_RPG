be6e2a90a8fc5837373e9cdde13d1816afdc02ad
jihe <https://github.com/>
2024-06-12 19:47:18 +0900

    플레이어 애니메이션 진행중

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class Move : MonoBehaviour
+{
+
+    public Animator moveAnimator;
+
+    private void Update()
+    {
+      
+    }
+
+    public void OnMove()
+    {
+        
+    }
+
+
+}
